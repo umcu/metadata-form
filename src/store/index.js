cfd2c2ca6a3c3172f5ccc513ee985065752cdfe5
Nico Poppelier <53787467+npoppelier@users.noreply.github.com>
2022-10-05 15:48:08 +0200

    Update index.js
    focus on metadata for Archivematica workflow

--- src/store/index.js
+++ src/store/index.js
@@ -4,57 +4,29 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     schema: {
-      AuthorName:             {label: 'Author name', type: 'text', required: true},
-      AuthorAffiliation:      {label: 'Author affiliation', type: 'text', required: true},
-      AuthorIdentifier:       {label: 'Author identifier', type: 'text', required: false},
-      AuthorIdentifierScheme: {label: 'Author identifier scheme', type: 'fixed', required: false, value: 'ORCID'},
-      ContactAffiliation:     {label: 'Contact affiliation', type: 'fixed', required: false, value: 'UMC Utrecht'},
-      ContactEmail:           {label: 'Contact email', type: 'menu', required: true},
-      ContactName:            {label: 'Contact name', type: 'text', required: true},
-      DepositDate:            {label: 'Deposit date', type: 'date', required: true},
-      DepositorDepartment:    {label: 'Depositor department', type: 'text', required: false},
-      DepositorDivision:      {label: 'Depositor division', type: 'menu', required: true},
-      DepositorName:          {label: 'Depositor name', type: 'text', required: true},
-      DescriptionText:        {label: 'Description', type: 'memo', required: false},
-      KeywordTerm:            {label: 'Keyword term', type: 'text', required: false},
-      KeywordVocabulary:      {label: 'Keyword vocabulary', type: 'text', required: false},
-      Language:               {label: 'Language', type: 'fixed', required: false, value: 'English'},
-      PublicationDate:        {label: 'Publication date', type: 'date', required: false},
-      Subject:                {label: 'Subject', type: 'fixed', required: false, value: 'Medicine, Health and Life Sciences'},
-      timePeriodCoveredEnd:   {label: 'Study period end', type: 'text', required: false},
-      timePeriodCoveredStart: {label: 'Study period start', type: 'text', required: false},
-      Title:                  {label: 'Title', type: 'text', required: true},
-    },
-    menu: {
-      ContactEmail: ['',
-        'DHS-Datamanagement@umcutrecht.nl',
-        'DatamanagementBeeld@umcutrecht.nl',
-        'DatamanagementDHL@umcutrecht.nl',
-        'DatamanagementDVF@umcutrecht.nl',
-        'IGD-datamanagers@umcutrecht.nl',
-        'DatamanagementJC@umcutrecht.nl',
-        'DatamanagementKinderen@umcutrecht.nl',
-        'dLAB-Datamanagement@umcutrecht.nl',
-        'her_dm@umcutrecht.nl',
-        'DatamanagementVrouwenBaby@umcutrecht.nl'
-      ],
-      DepositorDivision: ['',
-        'Heart and Lungs',
-        'Imaging & Oncology',
-        'Internal Medicine and Dermatology',
-        'Julius Center',
-        'Laboratories, Pharmacy & Biomedical Genetics',
-        'Neurosciences',
-        'Pediatrics',
-        'Surgical Specialties',
-        'Vital Functions',
-        'Women and Baby'
-      ]
+      authorName:             {label: 'Author name',        type: 'text',   required: true},
+      dateOfDeposit:          {label: 'Deposit date',       type: 'date',   required: true},
+      dsDescriptionValue:     {label: 'Description',        type: 'memo',   required: true},
+      keywordValue:           {label: 'Keyword term',       type: 'text',   required: true},
+      language:               {label: 'Language',           type: 'fixed',  required: false, value: 'English'},
+      timePeriodCoveredEnd:   {label: 'Study period end',   type: 'text',   required: false},
+      timePeriodCoveredStart: {label: 'Study period start', type: 'text',   required: false},
+      title:                  {label: 'Title',              type: 'text',   required: true},
+      datasetId:              {label: 'Related datasets',   type: 'text',   required: false},
+      typeOfResearch:         {label: 'Type of research',   type: 'choice', required: true,
+          options: [
+              {text: 'advanced therapeutic medicinal products', value: 30},
+              {text: 'investigational medicinal products',      value: 25},
+              {text: 'other WMO',                               value: 15},
+              {text: 'non-WMO',                                 value: 15},
+              {text: 'fundamental/pre-clinical',                value: 10}
+          ]},
     },
     valid: null,
     checked: null,
-    author: [1],
-    keyword: [1]
+    authorName: [1],
+    datasetId: [1],
+    keywordValue: [1]
   },
   mutations: {
       setchecked(state, payload) {
@@ -64,20 +36,28 @@ const store = new Vuex.Store({
           state.valid = payload
       },
       incauthor(state) {
-          const last = state.author[state.author.length-1];
-          state.author.push(last+1)
+          const last = state.authorName[state.authorName.length-1];
+          state.authorName.push(last+1)
       },
       inckeyword(state) {
-          const last = state.keyword[state.keyword.length-1];
-          state.keyword.push(last+1)
+          const last = state.keywordValue[state.keywordValue.length-1];
+          state.keywordValue.push(last+1)
+      },
+      increlated(state) {
+          const last = state.datasetId[state.datasetId.length-1];
+          state.datasetId.push(last+1)
       },
       decauthor(state) {
-          const n = state.author.length;
-          if (n > 1) { state.author.pop() }
+          const n = state.authorName.length;
+          if (n > 1) { state.authorName.pop() }
+      },
+      decrelated(state) {
+          const n = state.datasetId.length;
+          if (n > 1) { state.datasetId.pop() }
       },
       deckeyword(state) {
-          const n = state.keyword.length;
-          if (n > 1) { state.keyword.pop() }
+          const n = state.keywordValue.length;
+          if (n > 1) { state.keywordValue.pop() }
       },
   }
 })
