07e21beccd12c8d097b2aab2ea1e1957b3d098ed
Nico Poppelier <53787467+npoppelier@users.noreply.github.com>
2022-10-05 15:49:45 +0200

    Update FLabel.vue
    add dc.related; improve handling of repeatable fields

--- src/components/FLabel.vue
+++ src/components/FLabel.vue
@@ -1,10 +1,10 @@
 <template>
-  <label :for="f" :class="'col-form-label col-lg-'+w">
+  <label :for="f+postfix" :class="'col-form-label col-lg-'+w">
     {{flabel}}
-    <b-button v-if="btn" size="sm" class="py-0 my-0 px-1" @click="inc" variant="primary">
+    <b-button v-if="n==0" size="sm" class="py-0 my-0 px-1" @click="inc" variant="primary">
       <b-icon icon="file-plus"></b-icon>
     </b-button>
-    <b-button v-if="btn" size="sm" class="py-0 my-0 px-1" @click="dec" variant="primary">
+    <b-button v-if="n==0" size="sm" class="py-0 my-0 px-1" @click="dec" variant="primary">
       <b-icon icon="file-minus"></b-icon>
     </b-button>
   </label>
@@ -13,22 +13,24 @@
 <script>
 export default {
   name: 'FLabel',
-  props: ['f', 'w', 'btn'],
+  props: {'f': String, 'w': String, 'n': {type: Number, default: -1}},
   methods: {
     inc() {
-      const event = (this.f.startsWith('Author') ? 'incauthor' : 'inckeyword');
-      this.$store.commit(event)
-    },
+      const event = (this.f.startsWith('author') ? 'incauthor' :
+          (this.f.startsWith('keyword') ? 'inckeyword' :'increlated'));
+      this.$store.commit(event)},
     dec() {
-      const event = (this.f.startsWith('Author') ? 'decauthor' : 'deckeyword');
-      this.$store.commit(event)
-    }
-  },
+      const event = (this.f.startsWith('author') ? 'decauthor' :
+          (this.f.startsWith('keyword') ? 'deckeyword' :'decrelated'));
+      this.$store.commit(event)}},
   computed: {
+    postfix() { return this.n>=0 ? `#${this.n}` : ''},
     flabel() {
-      const field = this.$store.state.schema[this.f];
-      return field.label + (field.required ? ' *' : '')
-    }
-  }
-}
+      const state = this.$store.state;
+      const n = this.n, field = state.schema[this.f],
+            required = this.n < 1 ? state.schema[this.f].required : false;
+      if (n == -1) {
+        return field.label + (required ? ' *' : '')}
+      else {
+        return (n == 0 ? field.label : (n+1).toString()) + (required ? ' *' : '')}}}}
 </script>
